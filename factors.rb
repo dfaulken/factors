1f86a7a71a1bf2f73ddfb50eee047736da58036e
David Faulkenberry <david.faulkenberry@gmail.com>
2015-10-11 13:45:54 -0400

    Trimmed line length

--- factors.rb
+++ factors.rb
@@ -5,6 +5,7 @@ end
 n = ARGV[0] ? ARGV[0].to_i : 100
 k = (1..n).max_by{|x| factors(x).count}
 factors = factors(k)
-puts "The natural less than or equal to #{n} with the greatest number of factors is #{k}."
+puts "The natural less than or equal to #{n} with"
+puts "the greatest number of factors is #{k}."
 puts "#{k} has #{factors.count * 2} factors:"
 factors.each{|f| puts "#{k} = #{f} x #{k/f}"}
